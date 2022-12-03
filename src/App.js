a1620710d87ec755a646f0e5e886c22433a29777
Stephen Verdi <stephen.verdi000@gmail.com>
2022-12-02 23:34:07 -0500

    added sort by state and zip code buttons

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import DataLoader from './Components/DataLoader';
 import TestComponent1 from './Components/TestComponent1';
 import TargetValueSearchBar from './Components/TargetValueSearchBar';
 import SearchFieldDropdown from './Components/SearchFieldDropdown';
+import Visualization from './Components/Visualization';
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
@@ -12,7 +14,7 @@ function App() {
         <DataLoader />
         <SearchFieldDropdown />
         <TargetValueSearchBar />
-        
+        <Visualization />
       </header>
     </div>
   );
---
+++ src/Components/Visualization.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import Papa from 'papaparse';
+import { connect } from 'react-redux';
+import store from '../Store/store';
+
+class Visualization
+ extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            sortBy: "",
+            stateCounts: []
+        };
+        
+    }
+    createStateTable = () => {
+        var stateCodesArr = []
+        var data = this.props.data
+        console.log("data", data)
+        var stateCodeMapper = data.map(row => stateCodesArr.includes(row[6])|| row[6] === "state" ? null : stateCodesArr.push(row[6]))
+        
+        console.log("state codes array from mapping", stateCodesArr)
+        var stateCodesWithCounts = stateCodesArr.map(sc => [sc, 0])
+        console.log("pre count", stateCodesWithCounts)
+        stateCodesWithCounts.map(sc => data.map(row => row[6] === sc[0]? sc[1]++ : null))
+        console.log("state codes count array", stateCodesWithCounts)
+        this.setState({stateCounts: stateCodesWithCounts})
+    }
+    createZipTable = () => {
+        var zipCodesArr = []
+        var data = this.props.data
+        console.log("data", data)
+        var zipCodeMapper = data.map(row => zipCodesArr.includes(row[7])|| row[7] === "zip" ? null : zipCodesArr.push(row[7]))
+        
+        console.log("zip codes array from mapping", zipCodesArr)
+        var zipCodesWithCounts = zipCodesArr.map(zc => [zc, 0])
+        console.log("pre count", zipCodesWithCounts)
+        zipCodesWithCounts.map(zc => data.map(row => row[7] === zc[0]? zc[1]++ : null))
+        console.log("zip codes count array", zipCodesWithCounts)
+        this.setState({zipCounts: zipCodesWithCounts})
+    }
+    sortByState = () => {
+        console.log("sort by state")
+        this.setState({sortBy: "State"})
+        this.createStateTable()
+    }
+    sortByZip = () => {
+        console.log("sort by Zip")
+        this.setState({sortBy: "Zip"})
+        this.createZipTable()
+    }
+  render() {
+    const data = this.props.data
+    // console.log(inner)
+    return (
+        <div>
+            <input 
+            type="submit"
+            value="Sort by state"
+            onClick={this.sortByState}
+            />
+            <input 
+            type="submit"
+            value="Sort by ZIP Code"
+            onClick={this.sortByZip}
+            />
+            {this.state.sortBy === "State"? <div>{this.state.stateCounts}</div> : null}
+            {this.state.sortBy === "Zip"? <div>{this.state.zipCounts}</div> : null}
+
+    <h2>Visualization here</h2>;
+        </div>
+    )
+
+  }
+}
+function mapStateToProps(state) {
+  return {
+    data: state.data
+  }
+}
+
+export default connect(mapStateToProps)(Visualization);

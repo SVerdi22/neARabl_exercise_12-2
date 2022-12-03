9e6f75a2d36809a0a96c347b7ccdb7105fa0c9cf
Stephen Verdi <stephen.verdi000@gmail.com>
2022-12-02 21:57:44 -0500

    updated displays to mvp

--- src/Components/TargetValueSearchBar.js
+++ src/Components/TargetValueSearchBar.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import Papa from 'papaparse';
 import { connect } from 'react-redux';
 import store from '../Store/store';
+import Flexbox from 'flexbox-react';
 
 class TargetValueSearchBar extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            headerArr: [],
             input: "",
             results: []
         };
@@ -27,20 +29,32 @@ class TargetValueSearchBar extends Component {
     this.search(selectedField, this.state.input)
     }
 
-    search = (field, value) => {
+    search = async(field, value) => {
       console.log(value)
       var data = this.props.data
+      await this.setState({headerArr: data[0]})
       console.log(data[105][field])
       var resultRows = data.filter(row => row[field] === value)
       console.log("result rows", resultRows)
       // var results = resultRows.map(r => `${r[0], r[1], r[field]}`)
       // console.log(results)
+    
       this.setState({results: resultRows})
     }
     
   render() {
-    const data = this.props.data
-    // console.log(inner)
+    // const headerRow = this.props.data[0]
+    // console.log("data in render", headerRow)
+    console.log(this.state)
+    const styles = {
+      // border: "1px solid black",
+      width: "75%",
+      marginRight: "500px",
+      marginBottom: "200px",
+      marginLeft: "500px",
+      justifyContent: "center"
+      
+    }
     return(
         <>
         <h2>search field here</h2>
@@ -55,7 +69,41 @@ class TargetValueSearchBar extends Component {
         value="Submit"
         onClick={this.submitInput}
         />
-        <ul>{this.state.results}</ul>
+        {/* <ul>{this.state.results}</ul> */}
+        {this.state.results.length == 1 && this.props.field == 0? <div>
+          <img
+          src="https://raw.githubusercontent.com/jinchen003/Nearabl.Sample.Data/main/user.png"
+          alt="user icon"
+          />
+          {this.state.results.map(row => <tr>{row.map(val => <td>{val}</td>)}</tr> )}
+
+        </div>
+        : null}
+        {this.state.results.length == 1 && this.props.field == 2? <div>
+          <h2>company video here</h2>
+          <video controls width="100%">
+          <source src="https://raw.githubusercontent.com/jinchen003/Nearabl.Sample.Data/main/
+neARabl.mp4" type="video/mp4" />
+ <source src="https://raw.githubusercontent.com/jinchen003/Nearabl.Sample.Data/main/
+neARabl.mp4" type="video/webm" />
+            </video>
+            {this.state.results.map(row => <tr>{row.map(val => <td>{val}</td>)}</tr> )}
+
+          </div>
+          : null}
+        {this.state.results.length > 1 || this.props.field == 1 || this.props.field > 2? <div style={styles}> 
+        {this.props.field == 6 && this.state.results.length > 0? <div>{this.state.results.length} people in {this.state.results[0][6]}</div> : null}
+        <table>
+        <Flexbox>
+          <tbody>
+            {this.state.headerArr.map(val=> <td>{val}</td>)}
+            {this.state.results.map(row => <tr>{row.map(val => <td>{val}</td>)}</tr> )}
+          </tbody>
+        </Flexbox>
+        </table>
+         </div> 
+         :null}
+          
         </>
 
     ) 
